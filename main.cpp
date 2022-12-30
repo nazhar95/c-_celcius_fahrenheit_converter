9ae7b80b81cc71578cad38ff3670a219d862f217
Naz Azhar <57789624+nazhar95@users.noreply.github.com>
2022-12-30 08:28:19 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-// Online C++ compiler to run C++ program online
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
